fb94f2ee7f29fd5c0007490ea9b7c9bb70348b31
Sergei Gorobets <sergei.gorobets@vk.team>
2024-08-02 22:22:56 +0300

    lesson_5

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_menegment():
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.driver_name = 'firefox'
+    browser.config.driver_options = webdriver.FirefoxOptions()
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
+  #  browser.config.driver_options.add_argument('--headless')
+
+
+
+    yield
+
+    browser.quit()
---
+++ tests/test_forma.py
@@ -0,0 +1,35 @@
+import os.path
+
+from selene import browser, have, be
+
+
+def test_demoga_forms():
+    browser.open('/')
+    browser.element('#firstName').type('Sergei')
+    browser.element('#lastName').type('Gorobets')
+    browser.element('#userEmail').type('sergei.gorobets.vit@gmail.com')
+    browser.element('[for="gender-radio-1"]').click()
+    browser.element('#userNumber').type('79854910266')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__month-select option[value="9"]').click()
+    browser.element('.react-datepicker__year-select option[value="1996"]').click()
+    browser.element('.react-datepicker__day.react-datepicker__day--007').click()
+    browser.element('#subjectsInput').type('Biology').press_tab()
+    browser.element('[for="hobbies-checkbox-3"]').click()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('imeg.jpg'))
+    browser.element('#currentAddress').type('Moscow')
+    browser.element('#react-select-3-input').type('Haryana').press_enter()
+    browser.element('#react-select-4-input').type('Karnal').press_enter()
+    browser.element('#submit').click()
+    browser.element('.modal-content').element('table').all('tr').all('td').even.should(have.exact_texts(
+        'Sergei Gorobets',
+        'sergei.gorobets.vit@gmail.com',
+        'Male',
+        '7985491026',
+        '07 October,1996',
+        'Biology',
+        'Music',
+        'imeg.jpg',
+        'Moscow',
+        "Haryana Karnal"
+    ))
